f386b94284dcafd18b68edc3c3c1ec74f3cb4a40
Fadhilah Yuda Pratama <kimdayudapratama@gmail.com>
2026-07-25 05:40:57 +0700

    Menambahkan Props

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const name = "fadhilah Yuda Pratama";
   return (
     <div>
-      <Header />
+      <Header name="kim" age={25} />
       <h1>Hello {name}</h1>
       <p>Belajar JSX itu menyenangkan!</p>
 
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,5 +1,10 @@
-function Header() {
-    return <h1>Ini adalah component Header</h1>
+function Header({ name, age }) {
+    return (
+        <div>
+            <h1>halo {name} Ini adalah component Header</h1>
+            <p>Umur saya {age} tahun.</p>
+        </div>
+    );
 }
 
 export default Header
