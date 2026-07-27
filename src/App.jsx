ea9ce28829b587d0dc9bd25fa3c0f7cb6085f97c
Fadhilah Yuda Pratama <kimdayudapratama@gmail.com>
2026-07-27 12:46:53 +0700

    Belajar mengenai Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { ProfileContext } from './component/ProfileContext'
 // import './App.css'
 import Welcome from './component/Welcome';
 import Header from './component/Header';
@@ -14,47 +16,60 @@ import Memoization from './component/Memoization';
 
 function App() {
   const name = "fadhilah Yuda Pratama";
-
+  const [user, setUser] = useState({
+    name: "Fadhilah Yuda Pratama",
+    age: 30,
+    location: "Jakarta",
+    email: "fadhilahyudapratamawork@gmail.com",
+    description: "Seorang Frontend Developer",
+    available: true,
+    socialMedia: {
+      twitter: "@fadhilahyuda",
+      linkedin: "fadhilahyudapratama",
+    },
+  });
   return (
-    <div>
-      <Section>
-        <h1>Belajar JSX itu menyenangkan!</h1>
-        <small>JSX adalah singkatan dari JavaScript XML. JSX adalah ekstensi sintaks untuk JavaScript. JSX memungkinkan kita untuk menulis HTML di dalam React.</small>
-      </Section>
-      <Section>
-        <Welcome />
-      </Section>
-      <Section>
-        <Header />
-      </Section>
-      <Section>
-        <Profil name="Fadhilah Yuda Pratama" age={30} location="Jakarta" email="fadhilahyudapratama@example.com" description="Seorang developer yang senang belajar teknologi baru." available={true} socialMedia={{ twitter: "@fadhilahyuda", linkedin: "fadhilahyudapratama" }}  />
-      </Section>
-      <Section>
-        <Counter />
-      </Section>
-      <Section>
-        <Eventhandling />
-      </Section>
-      <Section>
-        <ConditionalRendering />
-      </Section>
-      <Section>
-        <ListKeys />
-      </Section>
-      <Section>
-        <Form />
-      </Section>
-      <Section>
-        <UseEffect />
-      </Section>
-      <Section>
-        <UseRef />
-      </Section>
-      <Section>
-        <Memoization />
-      </Section>
-    </div>
+    <ProfileContext.Provider value={user}>
+      <div>
+        <Section>
+          <h1>Belajar JSX itu menyenangkan!</h1>
+          <small>JSX adalah singkatan dari JavaScript XML. JSX adalah ekstensi sintaks untuk JavaScript. JSX memungkinkan kita untuk menulis HTML di dalam React.</small>
+        </Section>
+        <Section>
+          <Welcome />
+        </Section>
+        <Section>
+          <Header />
+        </Section>
+        <Section>
+          <Profil />
+        </Section>
+        <Section>
+          <Counter />
+        </Section>
+        <Section>
+          <Eventhandling />
+        </Section>
+        <Section>
+          <ConditionalRendering />
+        </Section>
+        <Section>
+          <ListKeys />
+        </Section>
+        <Section>
+          <Form />
+        </Section>
+        <Section>
+          <UseEffect />
+        </Section>
+        <Section>
+          <UseRef />
+        </Section>
+        <Section>
+          <Memoization />
+        </Section>
+      </div>
+    </ProfileContext.Provider>
   )
 }
 
--- src/component/Profil.jsx
+++ src/component/Profil.jsx
@@ -1,19 +1,34 @@
-function Profil({ name, age, location, email, description, available, socialMedia }) {
+import { useContext } from "react";
+import { ProfileContext } from "./ProfileContext";
+
+function Profil() {
+    const user = useContext(ProfileContext);
+     const {
+        name,
+        age,
+        location,
+        email,
+        description,
+        available,
+        socialMedia,
+    } = user;
     return (
         <div>
-            <h2>Belajar Props Terkait Profil</h2>
-            <small>Profil adalah informasi pribadi seseorang yang mencakup nama, umur, alamat, dan data lainnya.</small>
-            <p>Nama: {name}</p>
-            <p>Umur: {age}</p>
-            <p>Alamat: {location}</p>
-            <p>Email: {email}</p>
-            <p>Deskripsi: {description}</p>
-            <p>Tersedia: {available ? "Ya" : "Tidak"}</p>
-            <p>Media Sosial:</p>
-            <ul>
-                <li>Twitter: {socialMedia.twitter}</li>
-                <li>LinkedIn: {socialMedia.linkedin}</li>
-            </ul>
+        <h2>Belajar Context API</h2>
+
+        <p>Nama: {name}</p>
+        <p>Umur: {age}</p>
+        <p>Alamat: {location}</p>
+        <p>Email: {email}</p>
+        <p>Deskripsi: {description}</p>
+        <p>Tersedia: {available ? "Ya" : "Tidak"}</p>
+
+        <p>Media Sosial:</p>
+
+        <ul>
+            <li>Twitter: {socialMedia.twitter}</li>
+            <li>LinkedIn: {socialMedia.linkedin}</li>
+        </ul>
         </div>
     );
 }
